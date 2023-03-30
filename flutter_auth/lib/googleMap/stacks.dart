6ad83c4ec783b5894b0720f978f0cd338e48d97c
mihirbarad00 <126682125+mihirbarad00@users.noreply.github.com>
2023-03-30 18:52:40 +0530

    Create stacks.dart

---
+++ flutter_auth/lib/googleMap/stacks.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Stacks extends StatefulWidget {
+  const Stacks({super.key});
+
+  @override
+  State<Stacks> createState() => _StacksState();
+}
+
+class _StacksState extends State<Stacks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
